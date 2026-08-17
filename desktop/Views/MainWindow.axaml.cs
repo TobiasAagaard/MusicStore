fefaf62879d0ee8d9e8e4840c7a6a356711204cf
TobiasAagaard <tach@trifork.com>
2026-08-17 10:04:12 +0200

    Add initial implementation of MusicStore.Desktop

---
+++ desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MusicStore.Desktop.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
